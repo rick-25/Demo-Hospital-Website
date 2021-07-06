8f02d63d9edc62b08e3a312faa055e205c041e2c
Aditya Dwivedy <rick@pop-os.localdomain>
2021-07-06 09:50:51 +0530

    Search by pressing enter

--- javascript/searchbox.js
+++ javascript/searchbox.js
@@ -1,10 +1,18 @@
 function search() {
     let input = document.querySelector("div.search_box input").value;
-    
+
     //For invalid request
-    if(input.length == 0)
+    if (input.length == 0)
         return;
-    
+
     //open url in anpther tab
-    window.open("https://www.google.com/search?q="+input, "_blank");
+    window.open("https://www.google.com/search?q=" + input, "_blank");
+}
+
+function keypress() {
+    document.addEventListener('keydown', function (event) {
+        if (event.key === "Enter") {
+            search();
+        }
+    });
 }
