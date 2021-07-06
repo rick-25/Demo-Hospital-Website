d205be65990541a6f91ab64c75a45d08a49cf4e9
Aditya Dwivedy <rick@pop-os.localdomain>
2021-07-06 09:38:26 +0530

    Added search box

---
+++ javascript/searchbox.js
@@ -0,0 +1,10 @@
+function search() {
+    let input = document.querySelector("div.search_box input").value;
+    
+    //For invalid request
+    if(input.length == 0)
+        return;
+    
+    //open url in anpther tab
+    window.open("https://www.google.com/search?q="+input, "_blank");
+}
